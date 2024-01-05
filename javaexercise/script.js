c1bd17ab3d3754b86e78cae4b741b921947727d8
Sulu2011 <126379528+Sulu2011@users.noreply.github.com>
2024-01-05 18:39:31 +0400

    Add files via upload

---
+++ javaexercise/script.js
@@ -0,0 +1,88 @@
+//  let x=parseInt(prompt("eded daxil edin"))
+//for (let index = 1; index<11; index++) {
+///console.log(x*index)
+ //   
+//};//tapsiriq 1bitmisdir.
+//ikincisin ve ucuncusun ede bilmedim
+//let x=parseInt(prompt('yasinizi daxil edin'))
+//let yas=18
+//if(x<=18){
+  //  console.log( "Siz qanuni yaşdasınız")
+//}
+//else{
+  //  console.log("Siz yetkinlik yaşına çatmısınız")   
+//}
+
+
+//}//4cu tapsiriq bitmisdir
+
+//let x=parseInt(prompt('temperaturu daxil edin'))
+////let norma=18
+//if(x=>18){
+///    console.log( 'İsti havada çölə çıx')
+///}
+//else{
+ //   console.log("Çıxanda diqqətli olun")
+//}
+//5ci tapsiriq bitti
+//let x=parseInt(prompt('eded daxil etmeyinizi xahis edirem'))
+//let a=0
+//if(x>a){
+//  console.log( "Müsbət nömrə" )
+//}
+//else if(x<a){
+//  console.log("Mənfi nömrə")
+//}
+//else{
+//  console.log("sifir")
+////}
+//6ci tapssiriq bitmisdir.
+//let a=parseInt(prompt('eded daxil etmeyinizi xahis edirem'))
+//let b=parseInt(prompt('eded daxil etmeyinizi xahis edirem'))
+//function Hesablama(c,d){
+  ////let result=a*b
+//  return result;
+//}
+//console.log(Hesablama(a,b))
+//7ci tap bitmisdir.
+//let metn=prompt('mektubu daxil edin');
+//let arr=[]
+//let arr2=[]
+//  arr=metn.split('')
+//for(let i=0;i==arr.length;i++){
+//  if(arr[i]=='k'){
+//    arr2.push(k)
+  //}
+////
+//}
+//8ci tapsiriq bitmisdir
+//9,10,11,12de set timeout funksiyasin yaza bilmedim
+//let a=parseInt(prompt('eded deyin'))
+//let b=parseInt(prompt('eded deyin'))
+//console.log(Math.max(a,b))
+//console.log(Math.min(a,b))
+//13cu tapsiriq bitmisdir
+//let x=parseInt(prompt('eded daxil edin'))
+//if(x%2==0){
+//  console.log('eded cutdur')
+//}
+//else if(x==0){
+//console.log('eded 0dir')
+//}
+//else{
+ // console.log('eded tekdir')
+///}
+//14 bitmisdir
+//let arr=[]
+//let x=prompt('ne etmek istediyinizi deyin')
+//if(x=='elave etmek'){
+//  let a=prompt('adi deyin')
+ // arr.push(a)
+ // console.log(arr)
+//}
+//else if(x=='silmek'){
+ // let arr1=arr.delete
+//}
+//else{
+ // console.log(arr)
+//}tapsiriq 15 bitmisdir.
